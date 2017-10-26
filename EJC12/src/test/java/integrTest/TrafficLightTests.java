df30813af9894b43b7161517a0ff688f24f25b1a
groonyn <nicolasnn@gmail.com>
2017-10-26 19:30:10 +0300

    commit 19.29 , 26.10.2017

--- EJC12/src/test/java/integrTest/TrafficLightTests.java
+++ EJC12/src/test/java/integrTest/TrafficLightTests.java
@@ -6,31 +6,22 @@ import org.junit.Test;
 
 public class TrafficLightTests {
     TrafficLight trafficLight = new TrafficLight();
-    private String light;
-
     @Test
-    public void testTrafficLightGreenColour() {
-        light = trafficLight.chooseLight(6);
-        Assert.assertTrue(light == "Green Light!");
-        light = trafficLight.chooseLight(60);
-        Assert.assertTrue(light == "Green Light!");
+   void testTrafficLightGreenColour() {
+        Assert.assertEquals ("Green Light!", trafficLight.chooseLight(6));
+        Assert.assertEquals ("Green Light!", trafficLight.chooseLight(60));
     }
 
     @Test
-    public void testTrafficLightYellowColour() {
-        light = trafficLight.chooseLight(3);
-        Assert.assertTrue(light == "Yellow Light!");
-        light = trafficLight.chooseLight(5);
-        Assert.assertTrue(light == "Yellow Light!");
+   void testTrafficLightYellowColour() {
+        Assert.assertEquals ("Yellow Light!", trafficLight.chooseLight(3));
+        Assert.assertEquals ("Yellow Light!", trafficLight.chooseLight(5));
     }
 
     @Test
-    public void testTrafficLightRedColour() {
-        light = trafficLight.chooseLight(0);
-        Assert.assertTrue(light == "Red Light!");
-        light = trafficLight.chooseLight(2);
-        Assert.assertTrue(light == "Red Light!");
-
+   void testTrafficLightRedColour() {
+        Assert.assertEquals ("Red Light!", trafficLight.chooseLight(0));
+        Assert.assertEquals ("Red Light!", trafficLight.chooseLight(2));
     }
 
 }
