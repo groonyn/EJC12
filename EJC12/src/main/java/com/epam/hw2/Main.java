4081b8c697256436e9413e5ff06c4fcda18ecbed
groonyn <nicolasnn@gmail.com>
2017-10-29 23:04:36 +0300

    commit 29.10.2017, 23:04

--- EJC12/src/main/java/com/epam/hw2/Main.java
+++ EJC12/src/main/java/com/epam/hw2/Main.java
@@ -5,18 +5,12 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Main {
-    private static int enterMinute;
     public static void main(String[] args) throws IOException {
-    System.out.println("Enter minute: ");
-    BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-    String enteredMinute = reader.readLine();
-    try {
-        enterMinute = Integer.parseInt(enteredMinute);
-    } catch (IllegalArgumentException enterMinute) {
-        throw new IllegalArgumentException("You entered not integer number ");
+        System.out.println("Enter minute: ");
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String minuteNumber = reader.readLine();
+        System.out.println("Entered number of minutes = " + minuteNumber + " ,and you have "
+                + TrafficLight.chooseLight(minuteNumber));
     }
-        System.out.println(TrafficLight.chooseLight(enterMinute));
-
-        }
-    }
+}
 
--- EJC12/src/main/java/com/epam/hw2/TrafficLight.java
+++ EJC12/src/main/java/com/epam/hw2/TrafficLight.java
@@ -1,25 +1,41 @@
 package com.epam.hw2;
+
+import java.io.IOException;
+
 /**
  * Class TrafficLight taking numbers as input thru system input and giving Strings thru system output.
+ *
  * @author Nikolay Grunin
  * @version 1.0
  */
 public class TrafficLight {
+    public static int enteringMinute(String inputMinute) {
+        try {
+            return Integer.parseInt(inputMinute);
+        } catch (NumberFormatException e) {
+            System.err.println("Number is negative or not Integer");
+        }
+        throw new NullPointerException("Not a positive integer number");
+    }
+
     /**
      * Creates  method which takes
      *
-     * @param enterMinute and gives back string from chooseLight
+     * @param inputMinute and gives back string from chooseLight
      **/
-    public static String light;
 
-    public static String chooseLight(int enterMinute) {
-        if (enterMinute % 10 < 3 & enterMinute % 10 >= 1) {
-            return light = "Red Light!";
-        } else if (enterMinute % 10 > 2 & enterMinute % 10 < 6) {
-            return light = "Yellow Light!";
-        } else if (enterMinute % 10 > 5 & enterMinute % 10 <= 10 || enterMinute % 10 == 0) {
-            return light = "Green Light!";
-        } else
-            return light = "Negative number!";
+    public static String chooseLight(String inputMinute) throws IOException {
+        int minuteNumber = enteringMinute(inputMinute);
+        String lightColour = new String();
+        if (minuteNumber % 10 < 3 & minuteNumber % 10 >= 1) {
+            lightColour = "Red Light!";
+        } else if (minuteNumber % 10 > 2 & minuteNumber % 10 < 6) {
+            lightColour = "Yellow Light!";
+        } else if (minuteNumber % 10 > 5 & minuteNumber % 10 <= 10 || minuteNumber % 10 == 0) {
+            lightColour = "Green Light!";
+        } else {
+            throw new NullPointerException("Not a positive integer number");
+        }
+        return lightColour;
     }
 }
